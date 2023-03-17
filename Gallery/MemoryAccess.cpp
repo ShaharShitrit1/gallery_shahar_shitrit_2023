10c1674d7dc8b3436111acd6fc73319d1c18e844
shahar shitrit <shaharzivnoam@gmail.com>
2023-03-17 12:47:17 +0200

    Fixed listAlbums bug - now its printing the creation date

--- Gallery/MemoryAccess.cpp
+++ Gallery/MemoryAccess.cpp
@@ -14,7 +14,8 @@ void MemoryAccess::printAlbums()
 	std::cout << "Album list:" << std::endl;
 	std::cout << "-----------" << std::endl;
 	for (const Album& album: m_albums) 	{
-		std::cout << std::setw(5) << "* " << album;
+
+		std::cout << album.getCreationDate() << std::setw(5) << "* " << album;
 	}
 }
 
