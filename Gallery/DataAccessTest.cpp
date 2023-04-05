25944b5d7d07586f49eae6ca4a111045b83ae427
shahar shitrit <shaharzivnoam@gmail.com>
2023-04-05 13:13:54 +0300

    Added the addThreeUsers function

--- Gallery/DataAccessTest.cpp
+++ Gallery/DataAccessTest.cpp
@@ -4,3 +4,14 @@ bool DataAccessTest::open()
 {
     return DatabaseAccess::open();
 }
+
+bool DataAccessTest::AddThreeUsers()
+{
+    User user1(1, "shahar");
+    User user2(2, "my femily");
+    User user3(3, "hello");
+
+    DatabaseAccess::createUser(user1);
+    DatabaseAccess::createUser(user2);
+    DatabaseAccess::createUser(user3);
+}
